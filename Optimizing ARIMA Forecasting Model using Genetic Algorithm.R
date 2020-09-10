204d8d5c79dfb1fd6a6d7c35a8f30dfae481b7e2
Hadi Moheb-Alizadeh <50845777+hadi6314@users.noreply.github.com>
2020-09-10 02:56:29 -0400

    Update Optimizing ARIMA Forecasting Model using Genetic Algorithm.R

--- Optimizing ARIMA Forecasting Model using Genetic Algorithm.R
+++ Optimizing ARIMA Forecasting Model using Genetic Algorithm.R
@@ -1,184 +1,138 @@
-library('ggplot2')
-library('forecast')
-library('tseries')
-
-#daily_data = read.csv('day.csv', header=TRUE, stringsAsFactors=FALSE)
-
-day[,'date'] = as.Date(day$dteday)
-ggplot(day, aes(date,cnt)) + geom_line() + theme_bw()
-
-series = ts(day[,'cnt']) # to make a time series object
-day[,'clean_cnt'] = tsclean(series) # to remove outliers and imput missing data
-ggplot(day, aes(date,clean_cnt)) + geom_line() + theme_bw()
-ggplotly()
-
-
-day[,'weekly.moving'] = ma(day[,'clean_cnt'], order=7)
-day[,'monthly.moving'] = ma(day[,'clean_cnt'], order=30)
-ggplot() +
-  geom_line(data = day, aes(x = date, y = clean_cnt, colour = "Counts")) +
-  geom_line(data = day, aes(x = date, y = weekly.moving, colour = "Weekly Moving Average")) +
-  geom_line(data = day, aes(x = date, y = monthly.moving, colour = "Monthly Moving Average")) + theme_bw()
+#install.packages("rpart")
+#install.packages("caret")
+#install.packages("randomForest")
+#install.packages("e1071")
+#install.packages("rlang")
+library("e1071")
+library("randomForest")
+library(ISLR)
+library("rpart")
+library("caret")
+library("rlang")
+library(dplyr)
+library(drat)
+library(xgboost)
 
 
-series.weekly = ts(na.omit(day[,'weekly.moving']), frequency = 30)
-decom = stl(series.weekly, s.window="periodic")
-deseasonal_cnt = seasadj(decom)
-plot(decom)
+inTraining =  createDataPartition(Default$default, p = 0.8, list = FALSE)
+training = Default[inTraining,]
+testing = Default[-inTraining,]
 
-acf(series.weekly, main = '')
-pacf(series.weekly, main = '')
-
-diff.series = diff(deseasonal_cnt,  differences = 7)
-plot(diff.series)
-acf(diff.series, main = '')
-pacf(diff.series, main = '')
-
-
-fit = auto.arima(deseasonal_cnt, max.p = 5, max.q = 10, max.P = 20,
-                 max.Q = 20, max.order = 50, max.d = 20, max.D = 10, seasonal=TRUE)
-tsdisplay(residuals(fit),lag.max=45, main='(2,1,0) Model Residuals')
-fit2 = arima(deseasonal_cnt, order=c(1,1,7), seasonal = c(10,0,0))
-tsdisplay(residuals(fit2), lag.max=15, main='Seasonal Model Residuals')
-accuracy(fit)
-fcast = forecast(fit2, h=1)
-plot(fcast)
-arimaorder(fit)
-
-##### Google Project
-#GG[,'google.ts'] = GG[,'Close.Last'] %>% ts(frequency = 30) %>% tsclean()
-
-train.size = round(0.8*length(GG[,'Close.Last']))
-train.set = GG[1:train.size,'Close.Last'] %>% ts(frequency = 30) %>% tsclean() 
-test.set = GG[-(1:train.size),'Close.Last'] %>% ts(frequency = 30) %>% tsclean() 
-test.set.1 = as.data.frame(GG[-(1:train.size),'Close.Last'])
-colnames(test.set.1)[1] = 'test'
-
-#plot(train.set)
-#gg.decom = stl(train.set, s.window="periodic")
-#plot(gg.decom)
-
-gg.fit = auto.arima(GG[,'Close.Last'], seasonal=TRUE, allowdrift = T)
-far2 <- function(x,h){forecast(auto.arima(x, seasonal=TRUE, allowdrift = T), h=h)}
-ee = tsCV(ts(GG[,'Close.Last']), far2, h=1)
-sqrt(mean(ee^2, na.rm=TRUE))
-fitt = Arima(GG[,'Close.Last'], model=gg.fit, lambda=0)
-sqrt(mean((fitt$fitted - GG[,'Close.Last'])^2))
-
-
-
-google.ts = google[,'Close.Last'] %>% ts(frequency = 12) %>% tsclean()
-#fitt = Arima(google.ts, model=gg.fit, lambda=0)
-
-#gg.fit = auto.arima(google.ts, seasonal=TRUE, allowdrift = T)
-
-
-
-maxs = data.frame('max.p' = 2, 'max.d' = 2, 'max.q' = 2, 'max.P' = 2, 'max.D' = 2, 'max.Q' = 2)
-
-population.size = 20
-n.keep = round(population.size*0.5)
-mutation.number = round(0.2*ncol(population)*(nrow(population)-2))
-
-rank.probaibility = (n.keep - (1:n.keep) + 1) / sum(1:n.keep)
-cdf = function(probs) 
+# setting Cross-Validation Components. 
+rec.function = function(data, lev = NULL, model = NULL) 
 {
-  cm = 0
-  cmp = NULL
-  for (i in 1:length(probs))
+  metric = sensitivity(data$obs, data$pred, na.rm = TRUE, positive= "Yes")
+  names(metric) = "Recall"
+  metric
+}
+
+train.control = trainControl(method = "cv", number = 2,
+                             classProbs = TRUE, summaryFunction = rec.function) 
+
+# Fintness Function
+fitness = function(prm) #nt, mt, ns, mn
+{
+  model = train(default ~., data = Default, method = 'rf', trControl = train.control,
+                .n.trees = prm[1], .mtry = prm[2], .nodesize = prm[3], .maxnodes = prm[4], metric = "Recall")
+  return(-model$results[1,'Recall'])
+}
+
+# Simulated Annealing
+iteration = 0
+max.iteration = 2
+initial.tempreture = 1000
+final.tempreture = 900
+tempreture.updater = function(temp) {runif(1,0.8,0.95) * temp}
+
+initial.solution.generator = function() 
+{
+  no.tree = sample(seq(100,1000,1),1)
+  m = sample(seq(1:ncol(Default)),1)
+  node.size = sample(seq(1:50),1)
+  max.nodes = sample(seq(1:50),1)
+  return(c(no.tree,m,node.size,max.nodes))
+}
+
+neighbor = function(c.solution) 
+{
+  variable.loc = sample(1:4,1)
+  n.solution = duplicate(c.solution)
+  if (variable.loc == 1) 
   {
-    cm = cm + probs[i]
-    cmp[i] = cm
+    p.values = max(100,c.solution[variable.loc]-10):min(c.solution[variable.loc]+10, 1000)
+    n.solution[variable.loc] = sample(p.values[p.values != c.solution[variable.loc]],1)
   }
-  return(cmp)
-}
-
-cum.probs = cdf(rank.probaibility)
-
-# initial population
-population = cbind(p = sample.int(maxs[1,'max.p'], population.size, replace = TRUE), 
-                   d = sample.int(maxs[1,'max.d'], population.size, replace = TRUE),
-                   q = sample.int(maxs[1,'max.q'], population.size, replace = TRUE),
-                   P = sample.int(maxs[1,'max.P'], population.size, replace = TRUE),
-                   D = sample.int(maxs[1,'max.D'], population.size, replace = TRUE),
-                   Q = sample.int(maxs[1,'max.Q'], population.size, replace = TRUE)) %>% as.data.frame()
-
-
-# fitness function
-fitness = function(orders)  # p,d,q,P,D,Q
-{
-  #forecasting = function(x,h){forecast(arima(x, order = c(orders['p'],orders['d'],orders['q']), 
-  #seasonal=list(order=c(orders['P'],orders['D'],orders['Q']))), h=h)} #, period=30
-  
-  fit = Arima(google.ts, order =  c(orders['p'],orders['d'],orders['q']), 
-              seasonal = c(orders['P'],orders['D'],orders['Q']), hessian = FALSE)
-  #cv.fun = tsCV(google.ts, forecasting, h=1)
-  #cv.error = mean(cv.fun^2, na.rm = TRUE) %>% sqrt()
-  return(fit$bic) #cv.error
-}
-
-# Mating based on roulette wheel rank weighting
-mate = function(chroms) 
-{
-  rand.values = runif(population.size - n.keep)
-  selected = NULL
-  for (i in 1:length(rand.values))
+  else if (variable.loc == 2)
   {
-    selected[i] = length(cum.probs) - length(cum.probs[rand.values[i] < cum.probs]) + 1
+    p.values = max(1,c.solution[variable.loc]-5):min(c.solution[variable.loc]+5, ncol(Default))
+    n.solution[variable.loc] = sample(p.values[p.values != c.solution[variable.loc]],1)
   }
-  for (j in seq(1, length(selected), 2))
+  else if (variable.loc == 3)
   {
-    beta = runif(1)
-    alpha = runif(1)
-    chroms = rbind(chroms, round(beta * chroms[selected[j],] + (1-beta) * chroms[selected[j+1],]),
-                   round((1 - alpha) * chroms[selected[j],] + alpha * chroms[selected[j+1],]))
+    p.values = max(1,c.solution[variable.loc]-5):min(c.solution[variable.loc]+5, ncol(Default))
+    n.solution[variable.loc] = sample(p.values[p.values != c.solution[variable.loc]],1)
   }
-  rownames(chroms) = NULL
-  return(chroms)
-}
-
-# Mutation with the first two chromosomes unchanged
-mutation = function(chroms) 
-{
-  col.indexes = sample.int(length(chroms), mutation.number, replace = TRUE)
-  row.indexes = sample.int(nrow(chroms) - 2, mutation.number, replace = TRUE) + 2
-  for (i in 1:mutation.number)
+  else
   {
-    chroms[row.indexes[i], col.indexes[i]] = sample.int(maxs[1,col.indexes[i]], 1, replace = TRUE)
+    p.values = max(1,c.solution[variable.loc]-5):min(c.solution[variable.loc]+5, ncol(Default))
+    n.solution[variable.loc] = sample(p.values[p.values != c.solution[variable.loc]],1)
   }
-  return(chroms)
+  return(n.solution)
 }
 
-
-# The GA algorithm
-install.packages('compiler')
-library(compiler)
-
-cmp.fitness = cmpfun(fitness)
-fits = apply(population, 1, cmp.fitness)
-population = cbind(population, fit = fits)
-population = arrange(population, fit)
-convergence = 2^31
-while (convergence > 0.01) 
+transition = function(c.solution, n.solution, c.temp) 
 {
-  elits = population[1:n.keep,-length(population)]  
-  elit.fitness = population[1:2,'fit']
-  current.best = population[1,'fit']
-  population = mate(elits)
-  population = mutation(population)
-  non.elit.fitness = apply(population[3:nrow(population),], 1, cmp.fitness)
-  population = cbind(population, fit = cbind(elits.fitness,non.elit.fitness))
-  population = arrange(population, fit)
-  convergence =  abs(population[1,'fit'] - current.best)
+  transit = FALSE
+  if (fitness(c.solution) > fitness(n.solution)) {transit = TRUE}
+  else
+  {
+    acceptance.rate = runif(1)
+    if (exp(-(fitness(n.solution) - fitness(c.solution))/c.temp) > acceptance.rate) {transit = TRUE}
+  }
+  return(transit)
 }
 
 
-
-
-
-
-
-
-
-
-
+c.solution = initial.solution.generator()
+#c.fitness = fitness(c.solution)
+c.temp = initial.tempreture
+m = 0
+n.solution = neighbor(c.solution)
+uphill = 0
+reset.temp = c(initial.tempreture)
+while (c.temp > final.tempreture) 
+{
+  #n.fitness = fitness(n.solution)
+  while (m < max.iteration) 
+  {
+    if (transition(c.solution, n.solution, c.temp)) #& m <= max.iteration
+    {
+      uphill = 0
+      m = m + 1
+      c.solution =duplicate(n.solution)
+      n.solution = neighbor(c.solution)
+    }
+    else 
+    {
+      n.solution = neighbor(c.solution)
+      uphill = uphill + 1
+      if (uphill == 10)
+      {
+        beta = runif(1)
+        if (c.temp != reset.temp(0)) 
+        {
+          c.temp = beta * c.temp + (1 - beta) * reset.temp(0)
+          reset.temp = append(c.temp, 0)
+          uphill = 0
+        }
+        else 
+        {
+          c.temp = beta * c.temp + (1 - beta) * reset.temp[which(c.temp == sort(reset.temp)) + 1]
+          reset.temp = append(c.temp, 0)
+          uphill = 0
+        }
+      }
+    }
+  }
+  c.temp = tempreture.updater(c.temp)
+}
